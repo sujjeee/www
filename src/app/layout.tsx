212f54ccba6e52e1743cffd20b5d3b16768ef631
Suraj <sujjeee@outlook.com>
2023-12-21 22:05:43 +0530

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "@/styles/globals.css";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/providers/theme-provider";
