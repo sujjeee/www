ed3ed51efa9cf68201677df807405105e744674f
Suraj <sujjeee@outlook.com>
2024-10-27 22:38:23 +0530

    add content collection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const { withContentCollections } = require("@content-collections/next");
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  // your next.js config
+};
+ 
+// withContentCollections must be the outermost plugin
+module.exports = withContentCollections(nextConfig);
