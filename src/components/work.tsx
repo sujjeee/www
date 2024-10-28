f8c75ecc2a183c154df9323937ca6ffeba3ad5b3
Suraj <sujjeee@outlook.com>
2024-10-28 21:09:17 +0530

    add work component

---
+++ src/components/work.tsx
@@ -0,0 +1,37 @@
+import type React from "react"
+import { Icons } from "./icons"
+
+export interface WorkProps {
+  href: string
+  title: string
+  description: string
+  status?: "freelancing" | "contract work"
+}
+
+export function Work({ href, title, description, status }: WorkProps) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="group relative flex cursor-pointer flex-row items-center justify-between rounded-md duration-300 hover:before:absolute hover:before:-inset-2.5 hover:before:rounded-md hover:before:bg-accent/20 hover:before:content-['']"
+    >
+      <div className="flex flex-col space-y-1 z-10">
+        <div className="flex items-center space-x-2">
+          <span className="text-[15px] font-medium leading-4">{title}</span>
+          {status && (
+            <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-xs font-normal text-muted-foreground">
+              {status}
+            </div>
+          )}
+        </div>
+        <span className="text-[15px] prose prose-zinc dark:prose-invert">
+          {description}
+        </span>
+      </div>
+      <div className="transition duration-150 group-hover:rotate-45">
+        <Icons.link className="size-3.5 whitespace-nowrap text-muted-foreground" />
+      </div>
+    </a>
+  )
+}
