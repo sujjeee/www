56d3ba3eb010b97c90035c592a161ec50b11d9c4
Suraj <sujjeee@outlook.com>
2024-11-12 16:03:48 +0530

    updated paginations

--- src/components/sections/blog-pagination.tsx
+++ src/components/sections/blog-pagination.tsx
@@ -8,28 +8,26 @@ interface BlogPagination {
 
 export function BlogPagination({ nextPost, prevPost }: BlogPagination) {
   return (
-    <div className="flex justify-between">
-      <div className="flex items-center justify-between text-[14px]">
-        {prevPost && (
-          <Link
-            className="flex flex-col items-start"
-            href={`/${prevPost._meta.path}`}
-          >
-            <span className="mb-1 text-muted-foreground">Previous</span>
-            <span className="font-medium ">{prevPost.title}</span>
-          </Link>
-        )}
+    <div className="flex items-center justify-between text-[14px]">
+      {prevPost && (
+        <Link
+          className="flex flex-col items-start"
+          href={`/${prevPost._meta.path}`}
+        >
+          <span className="mb-1 text-muted-foreground">Previous</span>
+          <span className="font-medium ">{prevPost.title}</span>
+        </Link>
+      )}
 
-        {nextPost && (
-          <Link
-            className="flex flex-col items-start"
-            href={`/${nextPost._meta.path}`}
-          >
-            <span className="mb-1 text-muted-foreground">Next</span>
-            <span className="font-medium ">{nextPost.title}</span>
-          </Link>
-        )}
-      </div>
+      {nextPost && (
+        <Link
+          className="flex flex-col items-end text-right"
+          href={`/${nextPost._meta.path}`}
+        >
+          <span className="mb-1 text-muted-foreground">Next</span>
+          <span className="font-medium ">{nextPost.title}</span>
+        </Link>
+      )}
     </div>
   )
 }
